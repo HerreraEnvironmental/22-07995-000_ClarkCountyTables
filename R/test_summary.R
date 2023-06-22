82405acc13ac3ba697115d74c9a7565ab334feff
Regina Lionheart <rlionheart@herrerainc.com>
2023-06-22 16:23:52 -0700

    First draft of summary rows, client feedback.

---
+++ R/test_summary.R
@@ -0,0 +1,25 @@
+## Pick up from final.columnsN for this test run
+
+North <- table1 %>%
+  filter(str_detect(Watershed, "North")) 
+
+
+#test <- colSums(North[-10, -c(1:2)]) %>%
+test <- colSums(North[!str_detect(North$Reach, "Non-SMA"), -c(1:2)]) %>%
+  as.data.frame() %>%
+  rownames_to_column() %>%
+  pivot_wider(names_from = 1, values_from = 2) %>%
+  mutate(Watershed = "Total for Non-SMA Areas in Watershed:",
+         Reach = NA) %>%
+  select(Watershed, Reach, everything())
+
+final <- North %>%
+  rbind(test)
+
+
+
+
+
+
+         
+
